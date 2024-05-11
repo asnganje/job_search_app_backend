6fc5a61d6cd415ab567a40abdc49c8e3c04a4b82
asnganje <abd.nganje@gmail.com>
2024-05-11 20:36:02 +0300

    Add initial server app features (listen and get methods for basic routes)

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("JOB SEARCH PLATFORM");
+const express = require('express')
+const app = express()
+
+const port = 3000;
+
+app.get('/', (req,res)=> {
+    res.status(200).send('<h1>Server backend Home page</h1>');
+})
+
+app.get('/about', (req,res)=> {
+    const developer = {
+        name: "Abdul",
+        age: 38
+    }
+    res.status(200).json(developer)
+})
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on port ${port}...`);
+})
