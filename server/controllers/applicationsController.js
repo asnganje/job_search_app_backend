ce71dc52ce734b30494bfa436e2faf61997550dd
asnganje <abd.nganje@gmail.com>
2024-05-13 21:18:22 +0300

    Add basic applications controller CRUD logic

---
+++ server/controllers/applicationsController.js
@@ -0,0 +1,21 @@
+const getAllApplications = (req,res)=> {
+    res.send('Get all Applications')
+}
+
+const getApplication = (req,res)=> {
+    res.send('Get a single Application')
+}
+
+const createApplication = (req,res)=> {
+    res.send('Create an Application')
+}
+
+const updateApplication = (req, res)=> {
+    res.send('Update an Application')
+}
+
+const removeApplication = (req,res)=> {
+    res.send('Remove an Application')
+}
+
+module.exports = {getAllApplications, getApplication, createApplication, updateApplication, removeApplication}
