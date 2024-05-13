70edbf9b1c876431ffa36af9e498a24534fa18ac
asnganje <abd.nganje@gmail.com>
2024-05-13 21:19:51 +0300

    add users router route logic

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {
+        getAllUsers,
+        getUser,
+        createUser,
+        updateUser,
+        removeUser
+        } = require('../controllers/usersController')
+const router = express.Router()
+
+router.route('/').get(getAllUsers).post(createUser)
+router.route('/:id').get(getUser).patch(updateUser).delete(removeUser)
+
+module.exports = router
