643c455b4fb7e081da324762cdd2bfdbd65285d2
asnganje <abd.nganje@gmail.com>
2024-05-13 21:19:40 +0300

    add jobs router route logic

---
+++ server/routes/jobRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const {getAllJobs, 
+        getJob, 
+        createJob, 
+        updateJob, 
+        removeJob} = require('../controllers/jobsController') 
+
+router.route('/').get(getAllJobs).post(createJob)
+router.route('/:id').get(getJob).patch(updateJob).delete(removeJob)
+module.exports = router
