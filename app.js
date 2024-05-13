5a0cb72c05e44f801c4a0dbba0e9cb622b78214a
asnganje <abd.nganje@gmail.com>
2024-05-13 21:17:45 +0300

    Add middleware for handling three routes (jobs, users and applications ) and controller logic

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const app = express()
-
+const usersRouter = require('./server/routes/userRoutes')
+const applicationsRouter = require('./server/routes/applicationRoutes')
+const jobsRouter = require('./server/routes/jobRoutes')
 const port = 3000;
 
-app.get('/', (req,res)=> {
-    res.status(200).send('<h1>Server backend Home page</h1>');
-})
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/applications', applicationsRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
-app.get('/about', (req,res)=> {
-    const developer = {
-        name: "Abdul",
-        age: 38
-    }
-    res.status(200).json(developer)
-})
 
 app.listen(port, ()=> {
     console.log(`Server is listening on port ${port}...`);
