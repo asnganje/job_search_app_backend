55d2a6c3700e7da23af102f30b256e7ec7b2fbbb
asnganje <abd.nganje@gmail.com>
2024-05-14 06:09:37 +0300

    Add start function to spin up server using hidden db connection and port

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const usersRouter = require('./server/routes/userRoutes')
 const applicationsRouter = require('./server/routes/applicationRoutes')
 const jobsRouter = require('./server/routes/jobRoutes')
-const port = 3000;
+const connectDB = require('./server/db/connect')
 
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/applications', applicationsRouter)
 app.use('/api/v1/jobs', jobsRouter)
 
+const port = process.env.PORT
+const url = process.env.MONGO_URI
 
-app.listen(port, ()=> {
-    console.log(`Server is listening on port ${port}...`);
-})
+const start = async () => {
+    try {
+        await connectDB(url)
+        app.listen(port, ()=> {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
