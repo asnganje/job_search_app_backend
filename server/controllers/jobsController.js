6f6c70d16a5476171aafc68c92c7548654c1db35
asnganje <abd.nganje@gmail.com>
2024-05-15 16:48:38 +0300

    Add logic for creating a job and getting all jobs

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -1,13 +1,27 @@
-const getAllJobs = (req,res)=> {
-    res.send('Get all jobs')
+const Job = require("../models/Job")
+
+const getAllJobs = async (req,res)=> {
+    try {
+        const jobs = await Job.find({})
+        res.status(200).json({status:'success', data: jobs})
+    } catch (error) {
+        res.status(500).json({status:'fail', msg:error})
+    }
 }
 
 const getJob = (req,res)=> {
     res.send('Get a single job')
 }
 
-const createJob = (req,res)=> {
-    res.send('Create a job')
+const createJob = async (req,res)=> {
+    try {
+        const data = req.body
+        console.log(data);
+        const newJob = await Job.create(data)
+        res.status(201).json({status: 'success', data: newJob})
+    } catch (error) {
+        res.status(500).json({status:'fail', msg:error})
+    }
 }
 
 const updateJob = (req, res)=> {
