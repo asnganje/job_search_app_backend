4846475abf711812c3e4a08bbfbb195646099f04
asnganje <abd.nganje@gmail.com>
2024-06-05 16:43:05 +0300

    Modify user logic in model

--- server/models/User.js
+++ server/models/User.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default:'user'
     }
 })
 
