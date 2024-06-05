a4075ba308a5c75c0e65586449f796cd8b3101f8
asnganje <abd.nganje@gmail.com>
2024-06-05 16:42:47 +0300

    use http status code in user controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,53 +1,33 @@
 const User = require("../models/User")
+const {StatusCodes} = require('http-status-codes')
 
 const getAllUsers = async (req,res)=> {
-    try {
         const users = await User.find({})
-        res.status(200).json({status:'success', data: users})
-    } catch (error) {
-        res.status(500).json({status:'fail', msg: error})
-    }
+        res.status(StatusCodes.OK).json({status:'success', data: users})
 }
 
 const getUser = async (req,res)=> {
-    try {
         const {id: userID} = req.params
         const user = await User.findOne({_id: userID})
-        res.status(200).json({status: 'success', data: user})
-    } catch (error) {
-        res.status(500).json({status: 'fail', msg: error})
-    }
-
+        res.status(StatusCodes.OK).json({status: 'success', data: user})
 }
 
 const createUser = async (req,res)=> {
-    try {
         const user = req.body
         const newUser = await User.create(user)
-        res.status(201).json({status:'success', data: newUser})
-    } catch (error) {
-        res.status(500).json({status: 'fail', msg: error})
-    }
+        res.status(StatusCodes.CREATED).json({status:'success', data: newUser})
 }
 
 const updateUser = async (req, res)=> {
-    try {
         const {id: userID} = req.params
         const updatedUser = await User.findOneAndUpdate({_id: userID}, req.body, {new: true, runValidators: true})
-        res.status(200).json({status: 'success', data: updatedUser})
-    } catch (error) {
-        res.status(500).json({status: 'fail', msg: error})
-    }
+        res.status(StatusCodes.OK).json({status: 'success', data: updatedUser})
 }
 
 const removeUser = async (req,res)=> {
-    try {
         const {id: userID} = req.params
         await User.findOneAndDelete({_id: userID})
-        res.status(200).json({status: 'success', msg: 'User deleted successfully'})
-    } catch (error) {
-        res.status(500).json({status: 'fail', msg: error})
-    }
+        res.status(StatusCodes.OK).json({status: 'success', msg: 'User deleted successfully'})
 }
 
 module.exports = {getAllUsers, getUser, createUser, updateUser, removeUser}
