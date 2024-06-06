573bac6e92d7c862ec2c1acfc3c3f8b247b231c5
asnganje <abd.nganje@gmail.com>
2024-06-06 07:00:17 +0300

    Modigy job schema structure

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -7,12 +7,28 @@ const getAllJobs = async (req,res)=> {
 }
 
 const getJob = async (req,res)=> {
-        const {id: jobID} = req.params
-        const job = await Job.findOne({_id:jobID})
-        if(!job){
-            return res.status(500).json({msg: `No job with id: ${jobID} `})
+        const {title, industry, location} = req.query
+
+        if (!title && !industry && !location) {
+                return res.status(StatusCodes.BAD_REQUEST).json({status: 'fail', msg:'Please provide at least one search term'} )
         }
-        res.status(StatusCodes.OK).json({status: 'success', data: job})
+
+        let query={};
+        if (title) {
+                query.title = title
+        }
+        
+        if(industry) {
+                query.industry = industry
+        } 
+        if(location) {
+                query.location = location
+        }
+        const jobs = await Job.find(query)
+        if(jobs.length === 0){
+                return res.status(500).json({msg: `No jobs matching the criteria`})
+        }
+        res.status(StatusCodes.OK).json({status: 'success', data: jobs})
 }
 
 const createJob = async (req,res)=> {
